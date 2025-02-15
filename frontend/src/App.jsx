5b702dc485144ad2f45e605ffc4aecf10a61067e
DIGI <mercadoapploslugares@gmail.com>
2025-02-15 22:43:12 +0400

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+
+import digiLogo from '/digi.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="#" target="_blank">
+          <img style={{ width: '250px', height: '250px' }}
+          src={digiLogo} className="logo" alt="Digi logo"  />
+        </a>
+        
+      </div>
+      <h1>Добро пожаловать в DIGI!</h1>
+      
+    </>
+  )
+}
+
+export default App
